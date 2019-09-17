5ceb8331e9ff16ba9c1f570260a94b5a6bcb8465
Ariestya Dibyanugraha <ariestya.dibyanugraha@gmail.com>
2019-09-17 11:04:45 +0700

    Add project files.

---
+++ SimpleFetch/Helper.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Configuration;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Tooling.Connector;
+using System.Reflection;
+using System.Linq.Expressions;
+using Technosoft.Yana.XRM.AI.LDMSService.Entities;
+using System.Net;
+
+namespace SimpleFetch
+{
+    public static class HelperCode
+    {
+        public static IOrganizationService GetCrmService()
+        {
+            string username = ConfigurationManager.AppSettings["crm:Username"].ToString();
+            string password = ConfigurationManager.AppSettings["crm:Password"].ToString();
+            string region = ConfigurationManager.AppSettings["crm:Region"].ToString();
+            string orgUniqueId = ConfigurationManager.AppSettings["crm:OrgUniqueId"].ToString();
+            string crmuri = ConfigurationManager.AppSettings["crm:Uri"].ToString();
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            CrmServiceClient conn = new CrmServiceClient(username, CrmServiceClient.MakeSecureString(password), region, orgUniqueId,
+                useUniqueInstance: false, useSsl: false, orgDetail: null, isOffice365: true);
+
+            IOrganizationService service = conn.OrganizationWebProxyClient != null ? conn.OrganizationWebProxyClient : (IOrganizationService)conn.OrganizationServiceProxy;
+
+            return service;
+        }
+        
+        public static string GetAttributeLogicalName<E, P>(this E src, Expression<Func<E, P>> expression)
+        {
+            var member = expression.Body as MemberExpression;
+            if (member != null && member.Member is PropertyInfo)
+            {
+                var pi = member.Member as PropertyInfo;
+                var pn = pi.Name;
+
+                var epi = typeof(E).GetProperty(pn);
+                var attr = (AttributeLogicalNameAttribute)epi.GetCustomAttribute(typeof(AttributeLogicalNameAttribute));
+
+                if (attr == null)
+                {
+                    throw new System.ArgumentNullException(pn, "AttributeLogicalNameAttribute not found on property");
+                }
+
+                return attr.LogicalName;
+            }
+
+            throw new ArgumentException("Expression is not a Property");
+        }
+
+        public static EntityCollection GetEntityCollection(string FetchXML)
+        {
+            IOrganizationService service = HelperCode.GetCrmService();// (Username, Password);
+
+            EntityCollection results = service.RetrieveMultiple(new FetchExpression(FetchXML));
+
+            return results;
+        }
+
+        public static string CreateEntity(IOrganizationService service, Entity entity)
+        {
+            try
+            {
+                Guid Id = service.Create(entity);
+
+                return Id.ToString();
+            }
+            catch (Exception ex)
+            {
+
+                return ex.Message.ToString();
+            }
+        }
+        
+
+        public static string UpdateEntity(IOrganizationService service, Entity entity)
+        {
+            try
+            {
+                service.Update(entity);
+
+                return "Update Succeeded";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message.ToString();
+            }
+        }
+
+        public static IEnumerable<BusinessUnit> GetBusinessUnits(IOrganizationService service)
+        {
+            var bu = new BusinessUnit();
+
+            var colsbu = new ColumnSet(
+                bu.GetAttributeLogicalName(e => e.Id),
+                bu.GetAttributeLogicalName(e => e.BusinessUnitId),
+                bu.GetAttributeLogicalName(e => e.Name),
+                bu.GetAttributeLogicalName(e => e.msdyn_companycode)
+            );
+
+            var quExp = new QueryExpression(BusinessUnit.EntityLogicalName)
+            {
+                ColumnSet = colsbu ?? new ColumnSet(true)
+            };
+
+            quExp.Criteria.AddCondition(bu.GetAttributeLogicalName(c => c.lxs_ismaindealer), ConditionOperator.Equal, true);
+
+            var res = service.RetrieveMultiple(quExp).Entities;
+
+            return res.Any() ? res.Select(e => e.ToEntity<BusinessUnit>()) : Enumerable.Empty<BusinessUnit>();
+        }
+        
+        public static EntityCollection GetEntityCollection(IOrganizationService service, string FetchXML)
+        {
+            //IOrganizationService service = HelperCode.GetCrmService(Username, Password);
+
+            EntityCollection results = service.RetrieveMultiple(new FetchExpression(FetchXML));
+
+            return results;
+        }
+        public static xts_generalsetup GetGeneralSetup(IOrganizationService service, ColumnSet columnSet = null)
+        {
+            var query = new QueryExpression(xts_generalsetup.EntityLogicalName)
+            {
+                ColumnSet = columnSet ?? new ColumnSet(true),
+                PageInfo = new PagingInfo { PageNumber = 1, Count = 1 },
+                Criteria = { FilterOperator = LogicalOperator.And }
+            };
+
+            query.Criteria.AddCondition("statecode", ConditionOperator.Equal, 0);
+
+            var res = service.RetrieveMultiple(query).Entities;
+            if (res != null && res.Any())
+            {
+                return res[0].ToEntity<xts_generalsetup>();
+            }
+
+            return null;
+        }
+    }
+}
---
+++ SimpleFetch/Program.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+
+namespace SimpleFetch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test");
+            HelperCode.GetCrmService();
+            GetSomething();
+            Console.ReadLine();
+        }
+
+        static void GetSomething()
+        {
+            var fetchXML = string.Format(
+            @"
+            <fetch top='1' >
+                <entity name='xts_newvehicledeliveryorder' >
+                <attribute name='xjp_billtocustomerid' alias='CustId' />
+                <attribute name='xts_newvehicledeliveryordernumber' alias='NVDONo' />
+                <link-entity name='account' from='accountid' to='xjp_billtocustomerid' link-type='inner' >
+                    <attribute name='accountnumber' alias='CustNo' />
+                    <link-entity name='xts_customerclass' from='xts_customerclassid' to='xts_customerclassid' link-type='inner' >
+                    <attribute name='xts_accountreceiveabledim5from' alias='Dim5From' />
+                    <attribute name='xts_dimension5id' alias='Dim5Id' />
+                    <attribute name='xts_customerclass' alias='CustClass' />
+                    </link-entity>
+                </link-entity>
+                </entity>
+            </fetch>");
+
+            EntityCollection results = HelperCode.GetEntityCollection(fetchXML);
+
+            if (results.Entities.Count > 0)
+            {
+                foreach (var entity in results.Entities)
+                {
+                    if (entity.Attributes.Contains("Dim5From"))
+                    {
+                        Console.WriteLine(entity.FormattedValues["CustNo"]);
+                        Console.WriteLine(entity.FormattedValues["Dim5From"]);
+                        Console.WriteLine(entity.FormattedValues["CustClass"]);
+                        Console.WriteLine();
+                    }
+                }
+            }
+        }
+    }
+}
